4204616fee5cd04e96387f93d7fcdade044df349
Mark Caldropoli <mcaldro1@binghamton.edu>
2018-11-26 16:58:54 -0500

    Add files via upload

---
+++ Program 4/caldropoli_m_p4/Item.java
@@ -0,0 +1,25 @@
+package caldropoli_m_p4;
+
+public class Item {
+    private double ratio;
+    private int profit;
+    private int weight;
+
+    public Item(int weight, int profit) {
+        this.weight = weight;
+        this.profit = profit;
+        this.ratio = (profit*1.0)/weight;
+    }
+
+    public int getProfit() {
+        return profit;
+    }
+
+    public double getRatio() {
+        return ratio;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
